9ca0afed749f126708d32940a0ef72eba8dffdd0
LEEJS256 <68937552+LEEJS256@users.noreply.github.com>
2026-05-12 04:15:24 +0900

    feat(Tag) : 태그 추가

---
+++ Source/CrazyBingo/Utility/CB_NativeGameplayTag.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Utility/CB_NativeGameplayTag.h"
+
+UE_DEFINE_GAMEPLAY_TAG(TAG_ProblemType, "ProblemType");
