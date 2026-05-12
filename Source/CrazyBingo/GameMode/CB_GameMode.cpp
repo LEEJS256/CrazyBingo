d655badcc9d4f7e715b83ebb4e67906723eb45f3
LEEJS256 <68937552+LEEJS256@users.noreply.github.com>
2026-05-13 03:54:51 +0900

    fix(GM) : 마우스 고정

--- Source/CrazyBingo/GameMode/CB_GameMode.cpp
+++ Source/CrazyBingo/GameMode/CB_GameMode.cpp
@@ -18,11 +18,25 @@ void ACB_GameMode::BeginPlay()
 
 void ACB_GameMode::StartGame()
 {
-	if (!BingoBoardClass)
-		return;
 
 	APlayerController* PC = UGameplayStatics::GetPlayerController(this, 0);
-	if (!PC) return;
+	if (PC)
+	{
+		// 마우스 커서 보이게
+		PC->SetShowMouseCursor(true);
+        
+		// 마우스로 카메라 이동 막기
+		PC->SetIgnoreLookInput(true);
+		PC->SetIgnoreMoveInput(true);
+
+		// UI 인풋 모드로 변경
+		FInputModeUIOnly InputMode;
+		PC->SetInputMode(InputMode);
+	}
+	
+	if (!BingoBoardClass)
+		return;
+	
 
 	BingoBoard = CreateWidget<UCB_BingoBoard>(PC, BingoBoardClass);
 	if (BingoBoard)
