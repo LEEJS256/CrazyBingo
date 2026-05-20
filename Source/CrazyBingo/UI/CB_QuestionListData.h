8b35627ef268bd691586f0cbb939b479d4c7c9da
LEEJS256 <68937552+LEEJS256@users.noreply.github.com>
2026-05-21 04:44:44 +0900

    feat(Util) : 리스트뷰가 들고있는 퀴즈데이터

---
+++ Source/CrazyBingo/UI/CB_QuestionListData.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Data/CB_DataTable_Question.h"
+#include "UObject/NoExportTypes.h"
+#include "CB_QuestionListData.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CRAZYBINGO_API UCB_QuestionListData : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	// 리스트뷰 한 칸이 들고 있을 실제 퀴즈 데이터 구조체
+	FCB_DataTable_Question QuestionData;
+    
+	// 삭제나 관리를 위해 보관할 배열 인덱스 (옵션)
+	int32 ListIndex;
+};
