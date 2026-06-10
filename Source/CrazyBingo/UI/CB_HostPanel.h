dd8f2038cb83a9d6dd27c938e7915644e0256190
LEEJS256 <68937552+LEEJS256@users.noreply.github.com>
2026-06-11 04:00:40 +0900

    fix(UI): 호스트패널 유튜브및 해설 추가

--- Source/CrazyBingo/UI/CB_HostPanel.h
+++ Source/CrazyBingo/UI/CB_HostPanel.h
@@ -17,15 +17,19 @@ class CRAZYBINGO_API UCB_HostPanel : public UUserWidget
 public:
 	UPROPERTY(BlueprintReadWrite, Category = "Host")
 	class UCB_BingoBoard* TargetBingoBoard;
+
+
+
+	// int32 CurrentSelectedCellIndex = -1;
+
+	// 🌟 빙고판에서 칸을 누르면 호출되어 UI를 갱신할 수신 함수
+	void SetCurrentQuestionInfo(int32 CellIndex, const FCB_DataTable_Question& QuestionData);
+	
 protected:
 	// 위젯 블루프린트가 생성될 때 이벤트 바인딩을 처리하는 초기화 함수
 	virtual void NativeConstruct() override;
 
-	virtual FReply NativeOnMouseButtonDown(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent) override;
-	virtual FReply NativeOnMouseMove(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent) override;
-	virtual FReply NativeOnMouseButtonUp(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent) override;	
 
-	
 	UPROPERTY(meta = (BindWidget))
 	UButton* Btn_TeamA_Win;
 
@@ -35,6 +39,14 @@ protected:
 	UPROPERTY(meta = (BindWidget))
 	UButton* Btn_WrongAnswer;
 
+	UPROPERTY(meta = (BindWidget))
+	class UTextBlock* Text_CurrentQuestion;
+
+	UPROPERTY(meta = (BindWidget))
+	class UTextBlock* Text_Explanation;
+
+	UPROPERTY(meta = (BindWidget))
+	class UWebBrowser* WebBrowser_Youtube; 
 	// =========================================================================
 	// ⚡ 버튼 클릭 시 실행될 런타임 이벤트 함수들
 	// =========================================================================
@@ -48,6 +60,8 @@ protected:
 	void OnWrongAnswerClicked();
 
 private:
+	int32 CachedCellIndex = -1;
+	
 	// 🌟 드래그 시작 시 마우스 커서와 패널 좌상단 사이의 거리 기억용
 	FVector2D DragOffset;
 };
